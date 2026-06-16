04053c6876bd6c310948528b7402a01c225577a8
Daniel Jonker <ddavidjonker@outlook.com>
2026-06-16 17:19:41 +0200

    Add zenodo logo

--- app/components/providers/SourceLogo.vue
+++ app/components/providers/SourceLogo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="max-h-6 min-h-4">
-        <img v-if="props.type == 'CKAN'" class="h-6" src="/logos/ckan.svg" />
+        <img v-if="props.type == 'Zenodo'" class="h-6" src="/logos/zenodo.svg" />
         <img v-else-if="props.type == 'HuggingFace'" class="h-6" src="/logos/huggingface.svg" />
         <img v-else-if="props.type == 'Kaggle'" class="h-6" src="/logos/kaggle.svg" />
         <img v-else-if="props.type == 'GitHub'" class="h-6" src="/logos/github.svg">
